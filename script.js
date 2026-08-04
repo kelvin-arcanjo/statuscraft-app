dde13fc77377430a615f487fecf6e79180454bf2
kelvin_arcanjo0 <kelvinarcanjonzinga@gmail.com>
2026-08-05 00:09:54 +0100

    feat: adiciona simulacao de pre-visualizacao e validacao dinamica
    Adiciona placeholder no input de url, evento de submit para alterar o texto de pré-visualização de acordo com o destino, tratamento do evento de reset e validação em tempo real com classList.toggle

--- script.js
+++ script.js
@@ -1,10 +1,47 @@
 const form = document.querySelector('form')
 const inputVideo = document.querySelector('#video')
 const select = document.querySelector('#format')
+const preview = document.querySelector('#preview')
 
-form.addEventListener('submit' , function(event) {
+form.addEventListener('submit' , (event) => {
     event.preventDefault()
+
+    if (select.value === 'whatsapp') {
+        preview.textContent = 'Vídeo cortado para 30 segundos (Whatsapp Status).'
+
+    } else if (select.value === 'instagram') {
+        preview.textContent = 'Vídeo ajustado para o formato 9:16 (Instagram Reels).'
+
+    } else if (select.value === 'facebook') {
+        preview.textContent = 'Vídeo otimizado para o feed do Facebook.'
+
+    } else {
+        preview.textContent = 'Seleciona um destino válido...';
+    }
 })
+
+form.addEventListener('reset' , () => {
+    preview.textContent = 'A pré-visualização aparecerá aqui.'
+
+    inputVideo.classList.remove('invalido')
+})
+
+inputVideo.addEventListener('input' , () => {
+    const isValid = inputVideo.checkValidity()
+
+    inputVideo.classList.toggle('valido' , isValid)
+    inputVideo.classList.toggle('invalido' , !isValid)
+})
+
+select.addEventListener('change', () => {
+    const isValid2 = select.checkValidity();
+
+    select.classList.toggle('valido', isValid2);
+    select.classList.toggle('invalido', !isValid2);
+});
+
+
+
 
 
 
