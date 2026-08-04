a665dfc31b41923b49d769e3345ff1b22c5e99f8
kelvin_arcanjo0 <kelvinarcanjonzinga@gmail.com>
2026-08-04 22:14:12 +0100

    feat: conectado... script.js e adicionado captura do evento de submit
    Importado... o script.js no final do body do HTML, seleciona os elementos do formulário via querySelector e adiciona o preventDefault no evento de submit.

---
+++ script.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('form')
+const inputVideo = document.querySelector('#video')
+const select = document.querySelector('#format')
+
+form.addEventListener('submit' , function(event) {
+    event.preventDefault()
+})
+
+
+
+
